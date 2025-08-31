d505d22812c5ed9527f19493bc2573d38547ef2b
AlexisJoselyn <pfernandez.alexis@gmail.com>
2025-08-31 10:05:11 -0500

    chore: add notation repository in customer repository

--- customer-ms/src/main/java/com/bank/customerms/repository/CustomerRepository.java
+++ customer-ms/src/main/java/com/bank/customerms/repository/CustomerRepository.java
@@ -2,8 +2,11 @@ package com.bank.customerms.repository;
 
 import com.bank.customerms.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     boolean existsByDni(String dni);
     Optional<Customer> findByDni(String dni);
