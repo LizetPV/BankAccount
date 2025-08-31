6ddc491f84bf5e72e6ac16caf3b0de1f2ccd10b8
Yesenia Peche Vergaray <122371050+LizetPV@users.noreply.github.com>
2025-08-30 22:43:40 -0500

    Delete Bank directory

--- Bank/src/com/bank/domain/AccountType.java
+++
@@ -1,4 +0,0 @@
-package com.bank.domain;
-
-//se definen dos constantes para cuenta ahorro y cuenta corriente savings cuenta de ahorros y checking cuenta corriente
-public enum AccountType { SAVINGS, CHECKING }
