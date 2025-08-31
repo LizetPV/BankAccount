2e821e6017e1ab9b99397db1f3ef329bfb2c23ac
AlexisJoselyn <pfernandez.alexis@gmail.com>
2025-08-30 21:29:39 -0500

    chore: just to try

--- customer-ms/src/main/java/com/bank/customerms/CustomerMsApplication.java
+++ customer-ms/src/main/java/com/bank/customerms/CustomerMsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CustomerMsApplication {
-
+    //comentario de prueba para git
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerMsApplication.class, args);
 	}
